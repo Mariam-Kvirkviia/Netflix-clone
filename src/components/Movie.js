3cc6e447a4528dd9cb1ee9d4487a37ed89e0d6ee
Mariam Kvirkviia <marikanik1999@gmail.com>
2023-01-11 17:28:26 -0800

    slider

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,37 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 const Movie = (props) => {
   let movie = props.data;
+  let [heart, setHeart] = useState(false);
 
   return (
     <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-      <img
-        id={movie?.id}
-        src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`}
-        alt={movie?.title}
-      />
+      {movie?.backdrop_path ? (
+        <img
+          id={movie?.id}
+          src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`}
+          alt={movie?.title}
+        />
+      ) : (
+        ""
+      )}
+      <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+        <h3 className="flex justify-center items-center h-full">
+          {movie?.title}
+        </h3>
+
+        {!heart ? (
+          <FaRegHeart
+            className="absolute top-3 left-3"
+            onClick={() => setHeart(!heart)}
+          />
+        ) : (
+          <FaHeart
+            className="absolute top-3 left-3"
+            onClick={() => setHeart(!heart)}
+          />
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import Movie from "./Movie";
 const Row = ({ title, fetchURL }) => {
   let [movies, setMovies] = useState([]);
@@ -13,14 +14,25 @@ const Row = ({ title, fetchURL }) => {
   return (
     <>
       <h2 className="text-white fond-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
-        <div id="slider">
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          className="bg-white rounded-full left-0 absolute opacity-50 hidden z-10 hover:opacity-100 group-hover:block"
+          size={40}
+        />
+        <div
+          id="slider"
+          className=" w-full h-full overflow-x-scroll whitespace-nowrap scrollbar-hide scroll-smooth"
+        >
           {movies.length > 0
             ? movies.map((movie) => {
                 return <Movie data={movie} key={movie.id} />;
               })
             : null}
         </div>
+        <MdChevronRight
+          className="bg-white rounded-full right-0 absolute opacity-50 z-10 hidden hover:opacity-100 group-hover:block"
+          size={40}
+        />
       </div>
     </>
   );
